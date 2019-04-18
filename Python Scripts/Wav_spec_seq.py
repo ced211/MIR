8bc3eaadd79ad391cad761d88447c30929f21320
ced211 <cedricminh211@gmail.com>
2019-04-18 22:55:52 +0200

    +wav

---
+++ Python Scripts/Wav_spec_seq.py
@@ -0,0 +1,58 @@
+import numpy as np
+import os
+import json
+import keras
+from keras.utils import Sequence
+from keras.utils import to_categorical
+from random import shuffle
+import librosa
+
+class WavSpec(Sequence):
+    batch_size = 500
+    wav_shape = 0
+    spec_shape = 0
+    x_shape = 0
+    wav_dir = ""
+    spec_dir = ""
+
+    def __init__(self,spectrum_directory,wav_directory,labels_directory,batch_size = 100):
+        file = open(labels_directory + "\\instrument_families","r")
+        self.families = json.load(file)
+        self.filenames = list(self.families.keys())
+        shuffle(self.filenames)
+        self.wav_dir = wav_directory
+        self.spec_dir = spectrum_directory
+        wav,_ = librosa.load(self.wav_dir + self.filenames[0],mono=True,sr=None)
+        self.wav_shape = wav.shape
+        filename,_,_ = self.filenames[0].partition(".wav")
+        spec = np.load(self.spec_dir + filename + ".npy")
+        self.spec_shape = spec.shape
+        self.batch_size = batch_size
+        x = np.concatenate((np.reshape(spec,(-1,)), np.reshape(wav,(-1,))))
+        self.x_shape = x.shape
+        print("wav shape: " + str(self.wav_shape) + " spec shape " + str(self.spec_shape))
+        print("x shape: " + str(self.x_shape))
+    def __len__(self):
+        return int(np.ceil(len(self.filenames) / float(self.batch_size)))
+    
+    def __getitem__(self, index):
+        i = 0
+        while i < self.batch_size:
+            filename,_,_ = self.filenames[index].partition(".wav")
+            if i == 0:
+                spec_x = np.load(self.spec_dir + filename + ".npy")
+                wav_x,_ = librosa.load(self.wav_dir + filename + ".wav",mono=True,sr=None)
+                batch_x = np.concatenate((np.reshape(spec_x,(-1,)), np.reshape(wav_x,(-1))))
+                batch_y = self.families[self.filenames[index]]
+                self.x_shape = batch_x.shape
+                if wav_x.shape != self.wav_shape:
+                    print("wav shape is different in file " + filename)
+            spec_x = np.load(self.spec_dir + filename + '.npy')
+            wav_x,_ = librosa.load(self.wav_dir+filename+'.wav',mono=True,sr=None)
+            x = np.concatenate((np.reshape(spec_x,(-1,)), np.reshape(wav_x,(-1,))))
+            batch_x = np.vstack((batch_x,x))
+            batch_y =np.append(batch_y,self.families[self.filenames[index]])
+            i +=1
+        batch_y = to_categorical(np.array(batch_y),num_classes=11)
+
+        return batch_x,batch_y
---
+++ Python Scripts/nn_wav-spectrum.py
@@ -0,0 +1,82 @@
+import numpy as np
+from keras.layers import Input, Dense, Add, Lambda,Dropout,Flatten
+from keras.models import Model
+from Wav_spec_seq import WavSpec
+from keras.layers import Dense, Dropout,Reshape,Conv1D,MaxPooling1D
+from keras.models import load_model
+import keras
+import keras.backend as K
+if __name__ == "__main__":
+
+    train = WavSpec('../spectrum-train/','../audio/',"../nsynth-train")
+    valid = WavSpec('../spectrum-valid/','../../nsynth-valid/audio/',"../nsynth-valid")
+
+    #model on spectrum
+    input = Input(train.x_shape)
+    spec = train.spec_shape[0] * train.spec_shape[1]
+    spec_input = Lambda(lambda x: x[:,spec:], output_shape = (spec,))(input)
+    spec_input = Reshape((train.spec_shape[0],train.spec_shape[1],1))(spec_input)
+    base_model = load_model("../models/conv-spectrum/models-45.hdf5")
+    spectrum_model = Model(inputs=base_model.input, outputs=base_model.get_layer('dense_1').output)
+    spectrum_model.summary()    
+    spec_out = spectrum_model(spec_input)
+
+    """
+    #model on raw wav audio.
+    print("wav")
+    wav_input = Lambda(lambda x: x[:,:spec], output_shape = (train.wav_shape[0],)) (input) 
+    wav_input = Reshape((train.wav_shape[0],1)) (wav_input)
+    debug_model = Model(inputs=input,outputs=wav_input)
+    debug_model.summary()     
+    wav = Conv1D(16, kernel_size= 4, activation='relu', input_shape=(train.wav_shape[0],1)) (wav_input)
+    wav = Dropout(0.25)(wav)
+    wav = MaxPooling1D(pool_size=4)(wav)
+    wav = Conv1D(16, kernel_size= 4, activation='relu') (wav)
+    wav = Dropout(0.25)(wav)
+    wav = MaxPooling1D(pool_size=4)(wav)
+    wav = Conv1D(16, kernel_size= 4, activation='relu') (wav)
+    wav = Dropout(0.25)(wav)
+    wav = MaxPooling1D(pool_size=4)(wav)
+    wav = Flatten()(wav)
+    wav = Dense(128,activation = 'relu')(wav)
+    wav = Dropout(0.25)(wav)
+    """
+
+    #merge both model
+    """ x = keras.layers.concatenate([spec_out, wav])
+    x = Dense(128,activation = 'relu')(x)"""
+    predictions = Dense(11,activation = 'softmax')(spec_out)
+    model = Model(inputs= input, outputs=predictions)
+
+    #Only trained wav and top layer
+
+    #freeze spectrum weight
+    for layer in spectrum_model.layers:
+        layer.trainable = False  
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+    model.summary()
+
+    checkpoint = keras.callbacks.ModelCheckpoint("..\\models\\conv-spectrum\\models-{epoch:02d}" 
+        + ".hdf5", monitor='val_loss', 
+        verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+    model.fit_generator(train, epochs=25, verbose=1, validation_data=valid,callbacks=[checkpoint])
+
+    #fine tune
+    """ for layer in spectrum_model.layers:
+        layer.trainable = True
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+    checkpoint = keras.callbacks.ModelCheckpoint("..\\models\\wav-spectrum\\models-{epoch:02d}" 
+        + ".hdf5", monitor='val_loss', 
+        verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+    model.fit_generator(train, epochs=10, verbose=1, validation_data=valid,callbacks=[checkpoint])"""
+     
+
+
+        
+
