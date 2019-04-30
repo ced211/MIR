f1335c4c8254747867927789c4a81575f15aa059
Aubry Julie <43893665+AubryJulie@users.noreply.github.com>
2019-04-30 10:41:16 +0200

    Add files via upload

--- Python Scripts/plot_history.py
+++ Python Scripts/plot_history.py
@@ -4,4 +4,4 @@ import json
 if __name__ == "__main__":
     file = open(("..\\models\\conv-mfccs\\history "), "r")
     history = HistoryMem(history = json.load(file))
-    history.plot_history("conv-spectrum-time")
+    history.plot_history("conv-mfccs")
